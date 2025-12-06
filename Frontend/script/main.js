f103e0823c63ad26bf446f84f98d757b86fd3805
MateresDev <74964125+MateresDev@users.noreply.github.com>
2025-12-06 16:35:59 +0100

    add simple city selecting

---
+++ Frontend/script/main.js
@@ -0,0 +1,40 @@
+const cities = [
+    { left: 180, top: 120, name: "Warszawa" },
+    { left: 140, top: 140, name: "Łódź" }
+];
+
+const citiesParent = document.getElementById("cities");
+const infoWindow = document.getElementById("info-window");
+const infoHeader = document.getElementById("info-header");
+
+let selectedCity = "";
+
+refreshCities();
+
+function refreshCities() {
+    citiesParent.innerHTML = "";
+    for (let i = 0; i < cities.length; i++) {
+        let selectedColor = selectedCity == cities[i].name ? " selected-city" : "";
+        citiesParent.innerHTML +=
+            `<div class="city" id="` + cities[i].name + `" style="left: ` + cities[i].left + `px; top: ` + cities[i].top + `px">
+        <div class="city-point` + selectedColor + `" onclick="clickCity('` + cities[i].name + `')"></div>
+        <div class="city-name">` + cities[i].name + `</div>
+    </div>`;
+    }
+}
+
+
+
+
+function clickCity(city) {
+    infoWindow.style.display = "unset";
+    infoHeader.innerHTML = city;
+    selectedCity = city;
+    refreshCities();
+}
+
+function polandClick() {
+    selectedCity = "";
+    refreshCities();
+    infoWindow.style.display = "none";
+}
