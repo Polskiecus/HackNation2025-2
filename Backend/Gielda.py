9be295882f6b1db28eb07b159d61d19a84266bb5
Polskiecus <krzysztof.witkowski.raszynska@gmail.com>
2025-12-06 15:44:02 +0100

    started gielda bullshit

---
+++ Backend/Gielda.py
@@ -0,0 +1,19 @@
+class Akcja:
+
+	def __init__(self, nazwa: str, wartosc: float, remaining_shares: int):
+
+		self.nazwa            = nazwa
+		self.wartosc          = wartosc
+		self.remaining_shares = remaining_shares
+		self.shares_total     = shares_total
+
+
+class Scheduler:
+
+	def __init__(self, akcje: [Akcja]=[]):
+
+		akcje = akcje
+if __name__ == "__main__":
+
+
+	
