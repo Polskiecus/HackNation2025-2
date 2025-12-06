e7581afadce17ee717c06afccb250ba48185b3fb
Polskiecus <krzysztof.witkowski.raszynska@gmail.com>
2025-12-06 15:47:09 +0100

    added savestates

--- Backend/Gielda.py
+++ Backend/Gielda.py
@@ -1,3 +1,14 @@
+import json
+
+
+def loadAkcjaFromFile(path: str) -> Akcja:
+
+	f = open(path)
+	data = read().split()
+	f.close()
+
+	return Akcja(data[0], data[1], data[2], data[3])
+
 class Akcja:
 
 	def __init__(self, nazwa: str, wartosc: float, remaining_shares: int):
@@ -7,6 +18,15 @@ class Akcja:
 		self.remaining_shares = remaining_shares
 		self.shares_total     = shares_total
 
+	def __str__(self):
+
+		return f"{self.nazwa} {self.wartosc} {self.remaining_shares} {shares_total}"
+
+	def export(self, path: str):
+
+		f = open(path, "w")
+		f.write(self.__str__)
+		f.close()
 
 class Scheduler:
 
