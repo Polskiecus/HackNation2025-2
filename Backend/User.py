5de9221900b75d259a97d6eeac14772f13b3a664
tymon <tymon.jed.sub@gmail.com>
2025-12-06 18:22:35 +0100

    lol

--- Backend/User.py
+++ Backend/User.py
@@ -14,7 +14,7 @@ class User:
         self.id = id
 
     def __str__(self):
-        return r"{" + f"bilans: {self.bilans}, id: {id}, akcje: "json.dumps(self.akcje) + r"}"
+        return r"{" + f"bilans: {self.bilans}, id: {id}, akcje: " + json.dumps(self.akcje) + r"}"
 
     def get_bilans(self) -> (bool, float): #zwraca ile gracz ma kasy
         return (True, self.bilans)
