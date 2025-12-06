c1653cd91e428a0aa98983a0615f66b387695d43
tymon <tymon.jed.sub@gmail.com>
2025-12-06 21:56:11 +0100

    idk

--- Backend/main.py
+++ Backend/main.py
@@ -3,5 +3,5 @@ from Gielda import *
 from User import *
 from NewsHandler import *
 
-main_users: dict[str, User] = read_users_from_file("./Users/users.json") #para [login][uzytkownik]
+main_users: dict[str, User] = read_users_from_file("../Users/users.json") #para [login][uzytkownik]
 main_scheduler = Scheduler(loadAkcjeFromPath("../Firmy/"), 60) #to trzyma eventy i akcje
