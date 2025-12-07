60ed7e50dea1b1d8e20c555426707165b87b7a47
tymon <tymon.jed.sub@gmail.com>
2025-12-07 02:09:05 +0100

    me retarded

--- Backend/Listener.py
+++ Backend/Listener.py
@@ -164,7 +164,7 @@ async def Login(request: Request):
 		return "User does not exist!"
 
 	else:
-		if main_users[data["login"]]["password"] == data["password"]:
+		if main_users[data["login"]].password == data["password"]:
 			num_ = random.randint(0, 2**30)
 			while num_ in Cookies:
 				num_ = random.randint(0, 2**30)
